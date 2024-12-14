da70e9db6a6c745795c060201c90311bcb9a5f6e
Super60x <ejsdebakker@gmail.com>
2024-12-14 16:05:37 +0100

    upload File

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ const limiter = rateLimit({
 app.use(limiter);
 app.use(cors());
 app.use(express.json({ limit: '1mb' }));
-app.use(express.static(path.join(__dirname, 'frontend/build')));
+app.use(express.static(path.join(__dirname, 'frontend/dist')));
 
 // Enhanced logging middleware
 app.use((req, res, next) => {
@@ -219,8 +219,8 @@ app.get('/api/test-prompts', async (req, res) => {
 });
 
 // Add root route handler
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/dist', 'index.html'));
 });
 
 // Error handling middleware
